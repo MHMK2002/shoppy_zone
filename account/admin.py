3085253ccf46efd8ad5c0a70c6271c064b5f2540
h.khani <khanihasan1381@gmail.com>
2025-04-28 21:32:41 +0330

    Enhance UserAdmin configuration with additional fields, actions, and search capabilities

--- account/admin.py
+++ account/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
+from django.db.models import QuerySet
 
 from account.models import User
 
 
-# Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'first_name', 'last_name', 'date_joined']
+    ordering = ['username']
+    list_display = ['username', 'name', 'first_name', 'last_name', 'is_active']
+    list_editable = ['first_name', 'last_name', 'is_active']
+    list_display_links = ['username']
+    readonly_fields = ['username']
+    search_fields = ['username', 'first_name', 'last_name']
+    list_filter = ['is_active', 'is_superuser']
     fieldsets = (
         (
             'General',
@@ -29,3 +35,20 @@ class UserAdmin(admin.ModelAdmin):
             }
         )
     )
+    actions = ['make_admin', 'make_user_clear']
+
+    @admin.display()
+    def name(self, obj: User):
+        return f'{obj.first_name} {obj.last_name}' if obj.first_name or obj.last_name else None
+
+    def get_empty_value_display(self):
+        return '(none)'
+
+    @admin.action(description='Make admin')
+    def make_admin(self, request, queryset):
+        queryset.update(is_superuser=True)
+
+    @admin.action(description='Make user clear')
+    def make_user_clear(self, request, queryset: QuerySet[User]):
+        queryset.update(last_login=None, first_name='', last_name='')
+
