1520e1b15cff310943466fcce417c655af3652be
h.khani <khanihasan1381@gmail.com>
2025-05-10 22:03:28 +0330

    Enhance ProductListView with ordering and search capabilities

--- product/views.py
+++ product/views.py
@@ -1,10 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.pagination import PageNumberPagination
 
 from product.models import Category, Product
 from product.serializers import CategorySerializer, ProductSerializer
@@ -22,10 +23,13 @@ class CategoriesView(APIView):
 class CustomPagination(PageNumberPagination):
     page_size_query_param = 'size'
 
+
 class ProductListView(ListAPIView):
     permission_classes = [AllowAny]
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['category']
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = CustomPagination
+    filter_backends = [OrderingFilter, DjangoFilterBackend, SearchFilter]
+    ordering_fields = ('price',)
+    filterset_fields = ['category_id']
+    search_fields = ['title', 'slug']
+    queryset = Product.objects.all()
